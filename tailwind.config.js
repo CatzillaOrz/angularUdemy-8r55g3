973c149ea4cd17cbb6cad41f067f2a4fa3705100
CatzillaOrz <catzillaorz@outlook.com>
2022-07-12 12:30:10 +0800

    3 files modified, 2 added

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+module.exports = {
+  // Tailwind Paths
+  configJS: './tailwind-config.js',
+  sourceCSS: './src/tailwind.css',
+  outputCSS: './src/styles.css',
+  // Sass
+  sass: false,
+  // PurgeCSS Settings
+  // ...
+  theme: {
+    colors: {
+      indigo: '#5c6ac4',
+      blue: '#007ace',
+      red: '#de3618',
+    },
+  },
+};
