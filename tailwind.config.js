c6e0796a7d0bdf05ea9a3fb3264e072ec906f59b
CatzillaOrz <catzillaorz@outlook.com>
2022-07-12 12:43:13 +0800

    1 file modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,7 @@
-// tailwind.config.js
 module.exports = {
-  // Tailwind Paths
-  configJS: './tailwind.config.js',
-  sourceCSS: './src/tailwind.css',
-  outputCSS: './src/styles.css',
   content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
-  // Sass
-  sass: false,
-  // PurgeCSS Settings
-  // ...
   theme: {
-    colors: {
-      indigo: '#5c6ac4',
-      blue: '#007ace',
-      red: '#de3618',
-    },
+    extend: {},
   },
+  plugins: [],
 };
